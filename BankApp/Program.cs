f61c1643e9f83a550672ca8f482872f6e1798189
Imran <imran.sadik@outlook.com>
2022-10-25 16:32:14 -0400

    Add methods and constructor

--- BankApp/Program.cs
+++ BankApp/Program.cs
@@ -5,8 +5,16 @@
         static void Main(string[] args)
         {
            var myAccount = new Account();
-            myAccount.AccountNumber = 123456;
+           // myAccount.AccountNumber = 123456;
             myAccount.AccountName = "My checking";
+           // myAccount.Balance = 10000000000000000;
+            myAccount.Deposit(1001);
+            Console.WriteLine($"AN: {myAccount.AccountNumber}, Balance: {myAccount.Balance}, CD: {myAccount.CreatedDate}");
+
+            var myAccount2 = new Account();
+            Console.WriteLine($"AN: {myAccount2.AccountNumber}, Balance: {myAccount2.Balance}, CD: {myAccount2.CreatedDate}");
+
+
         }
     }
 }
