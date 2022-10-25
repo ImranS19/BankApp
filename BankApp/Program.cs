987e54aa6b5964e4c6b43cddeeb5801e2a129706
Imran <imran.sadik@outlook.com>
2022-10-25 15:24:52 -0400

    Initial commit

---
+++ BankApp/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace BankApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           var myAccount = new Account();
+            myAccount.AccountNumber = 123456;
+            myAccount.AccountName = "My checking";
+        }
+    }
+}
